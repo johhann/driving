9b9a6e772a6fc8ead4c6196d0a642b847e94a31e
Yohannes Mekonnen <johannesm1219@gmail.com>
2022-08-22 21:50:57 +0300

    something controller

---
+++ app/Http/Controllers/SomethingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SomethingController extends Controller
+{
+    /**
+     * Return something
+     */
+    public function sth()
+    {
+        return 'something';
+    }
+}
